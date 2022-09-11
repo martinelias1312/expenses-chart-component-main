58eb25aac404f06031c0296e723ea3daff06589d
= <martin.elias@unite.sk>
2022-09-11 17:18:49 +0200

    code refactoring

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const url = "http://myjson.dit.upm.es/api/bins/564m";
-const daysList = document.querySelectorAll("li");
+const spendingGraph = document.querySelector(".spending-graph");
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const date = new Date();
 const today = weekday[date.getDay()].toLowerCase();
@@ -11,29 +11,43 @@ const getData = function () {
     .then((res) => res.json())
     .then(function (data) {
       data.forEach(item => {
+        const container = document.createElement("li");
+
+        // create bar element with class, id and style
         const bar = document.createElement("div");
         bar.className = "bar";
         bar.id = item.day + `-bar`;
-        bar.style.height = item.amount * 3 + "px";
+        bar.style.height = item.amount * 2.5 + "px";
+
+        // create p element with class, id and style
+        const day = document.createElement("p");
+        day.className = "day";
+        day.innerText = item.day;
 
         // condition to check which day is today, and change color of its bar
         if(today.includes(item.day)) {
           bar.classList.add("today");
         }
 
-        // append new bar div to every list item
-        daysList.forEach(listItem => {
-          if(listItem.innerText === item.day) {
-            listItem.appendChild(bar);
-          } else return;
-        })
-      });
+        // create bar amount element with text
+        const barAmount = document.createElement("p");
+        barAmount.classList.add("bar-amount");
+        barAmount.innerText = "$" + item.amount;
 
-      const bars = [...document.querySelectorAll('.bar')];
-      bars.map(item => {
-        item.addEventListener("mouseover", () => {
-          console.log(item)
-        });
+        // append elements to container
+        spendingGraph.appendChild(container);
+        container.appendChild(bar);
+        container.appendChild(day);
+        container.appendChild(barAmount);
+
+        // mouseover event
+        bar.addEventListener("mouseover", () => {
+          barAmount.classList.add("show");
+        })
+
+        bar.addEventListener("mouseleave", () => {
+          barAmount.classList.remove("show");
+        })
       });
     })
     .catch((err) => console.log(err.mesage));
