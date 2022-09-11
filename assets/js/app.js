6fe233af99021112085af42c7c95062c50d7b013
= <martin.elias@unite.sk>
2022-09-11 14:50:29 +0200

    days const added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
 "use strict";
 
 const url = "http://myjson.dit.upm.es/api/bins/564m";
+const days = document.querySelectorAll("li");
 
 const getData = function () {
   fetch(url, { cache: "no-cache" })
     .then((res) => res.json())
-    .then(console.log(data))
+    .then(data => console.log(data))
     .catch((err) => console.log(err.mesage));
 };
 
